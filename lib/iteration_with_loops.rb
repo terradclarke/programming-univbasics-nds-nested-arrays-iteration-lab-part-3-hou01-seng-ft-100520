55df99cc359063b9d6ddf2624b71be899d61b9a0
Terranie Sims <terranie67@gmail.com>
2020-09-13 16:29:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row_index < src.count do 
     element_index = 0 
     while element_index < src[row_index].count do 
-      if src[row_index][element_index] != Integer   
+      if src[row_index][element_index].is_a? String    
       nested_strings << src[row_index][element_index]
       end 
       element_index += 1 
